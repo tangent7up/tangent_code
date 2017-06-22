d2eca8eaa80c53099324364729f7498fbd361a4e
tangent7up <tangent7up@163.com>
2017-06-22 09:01:35 +0800

    OK

--- ctr/ctr_spark.py
+++ ctr/ctr_spark.py
@@ -9,10 +9,11 @@ import math
 from pyspark.sql.session import SparkSession
 from pyspark.sql import Row
 from pyspark.sql.types import StructType,StringType,IntegerType
-from pyspark.ml.linalg import Vectors
 from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.linalg import SparseVector
 import json
 import sys
+import time
 
 def toSparse(l):
     d={}
@@ -27,37 +28,28 @@ def split(string,sign):
     else:
         return string.split(sign)
 
+
 def rowTransform(row):
-    new_row=[]
-    for column in full_columns+["target"]:
-        if column in main_key:
-            new_row.append(row[column])
-        if column in quantity:
-            new_row.append(row[column])
+    index_column = []
+    index = 0
+    for column in full_columns:
         if column in catagory:
-            new_row+=oneHot(columns_dict[column].get(row[column],0),columns_dict_len[column])
+            v=row[column]
+            if v=="":
+                new = columns_dict[column].get("_null_", None)
+            else:
+                new=columns_dict[column].get(row[column], None)
+            if new!=None:
+                index_column.append(index+new)
+            index+=columns_dict_len[column]
         if column in quality:
             words=split(row[column],"|")
-            index=[columns_dict[column].get(i,None) for i in words if columns_dict[column].get(i,None) is not None]
-            new_row+=oneHot2(index,columns_dict_len[column])
-        if column in target:
-            new_row.append(row[column])
-    return Row(label=new_row[-1], features=Vectors.sparse(len(new_row)-2 , toSparse(new_row[1:-1])))
-        
-def oneHot(index,length):
-    l=[0]*length
-    if index==0: pass
-    else: l[index-1]=1
-    return l
-    
-def oneHot2(index,length):
-    l=[0]*length
-    for i in index:
-        try:
-            l[i]=1
-        except:
-            pass
-    return l
+            choosen=[columns_dict[column].get(i, None) for i in words]
+            news=[index+i for i in choosen if choosen is not None]
+            index_column+=sorted(news)
+            index += columns_dict_len[column]
+    return Row(label=row["target"], features=SparseVector(index, index_column, [1]*len(index_column)))
+
 
 def spark_read(path,schema=None,header=False):
     df=spark.read.csv(path[0],schema=schema, header=header)
@@ -109,7 +101,7 @@ if __name__ == "__main__":
     schema_str = schema
     del (schema)
 
-
+    _time=time.time()
     if header=="yes":
         df_feature_train = spark_read(train_featuredata_path, header=True)
         df_click_train = spark_read(train_clickdata_path)
@@ -121,8 +113,10 @@ if __name__ == "__main__":
             schema=schema.add(column,StringType(),True)
         df_feature_train = spark_read(train_featuredata_path, schema=schema,header=False)
         df_click_train = spark_read(train_clickdata_path)
-
+    print("reading: ", str(time.time() - _time) + "s")
+    _time=time.time()
     check_bar=df_feature_train.rdd.map(lambda r: [int("|" in str(i)) for i in list(r)]).reduce(lambda ra,rb: [ra[i]+rb[i] for i in range(len(ra))])
+    print("check_bar: ", str(time.time() - _time) + "s")
 
 
     if mode == "train_test":
@@ -135,7 +129,6 @@ if __name__ == "__main__":
         check_bar_test=df_feature_test.rdd.map(lambda r: [int("|" in str(i)) for i in list(r)]).reduce(lambda ra,rb: [ra[i]+rb[i] for i in range(len(ra))])
         check_bar=[check_bar[i] + check_bar_test[i] for i in range(len(check_bar))]
 
-
     main_key=[key]
     quality = []
     catagory = []
@@ -147,26 +140,33 @@ if __name__ == "__main__":
             catagory.append(full_columns[i])
     target = ["target"]
 
-
+    _time = time.time()
     tran_cus=set(df_click_train.rdd.map(lambda r: r["_c0"]).collect())
+    print("click_set_broadcate: ", str(time.time() - _time) + "s")
     tran_cus_b = sc.broadcast(tran_cus)
     schema=df_feature_train.schema
     new_schema=schema.add("target",IntegerType(),True)
-    df_feature_train_ = df_feature_train.rdd.map(lambda r: list(r) +[int(r[key] in tran_cus_b.value)])
-    df_train=spark.createDataFrame(df_feature_train_,schema=new_schema)
+    _time = time.time()
+    # df_feature_train_ = df_feature_train.rdd.map(lambda r: list(r) +[int(r[key] in tran_cus_b.value)])
+    # df_train=spark.createDataFrame(df_feature_train_,schema=new_schema)
+    df_train = spark.createDataFrame(df_feature_train.rdd.map(lambda r: list(r) +[int(r[key] in tran_cus_b.value)]), schema=new_schema)
+    print("setting_target: ", str(time.time() - _time) + "s")
     if mode == "train_test":
         tran_cus = set(df_click_test.rdd.map(lambda r: r["_c0"]).collect())
         tran_cus_b = sc.broadcast(tran_cus)
-        df_feature_test_ = df_feature_test.rdd.map(lambda r: list(r) + [int(r[key] in tran_cus_b.value)])
-        df_test = spark.createDataFrame(df_feature_test_, schema=new_schema)
+        # df_feature_test_ = df_feature_test.rdd.map(lambda r: list(r) + [int(r[key] in tran_cus_b.value)])
+        # df_test = spark.createDataFrame(df_feature_test_, schema=new_schema)
+        df_test = spark.createDataFrame(df_feature_test.rdd.map(lambda r: list(r) + [int(r[key] in tran_cus_b.value)]), schema=new_schema)
 
 
     #确定需要做Onehot操作的维度
     print("确定需要做Onehot操作的维度")
     new_columns=[key]
     columns_dict = {}
+    _time = time.time()
     distinct_words1=df_train.select(catagory).rdd.flatMap(parse_feature_data1).distinct().collect()
     distinct_words2=df_train.select(quality).rdd.flatMap(parse_feature_data2).distinct().collect()
+    print("one_hot_columns: ", str(time.time() - _time) + "s")
     for column in catagory+quality:
         columns_dict[column]=[]
     for column in catagory:
@@ -181,23 +181,32 @@ if __name__ == "__main__":
     print("生成onehot后新的列")
     columns_dict_len={}
     for column in catagory:
-        unique=[i for i in list(set(columns_dict[column])) if i !=""]
-        columns_dict[column]=dict(zip(unique,range(1,1+len(unique))))
+        unique=[i for i in sorted(set(columns_dict[column])) if i !=""]
+        columns_dict[column]=dict(zip(unique,range(0,len(unique))))
         columns_dict_len[column]=len(unique)
         columns_dict[column][""]=0
         new_columns+=[column+"&&"+str(c) for c in unique]
     for column in quality:
-        unique=[i for i in list(set(columns_dict[column])) if i!=""]
+        unique=[i for i in sorted(set(columns_dict[column])) if i!=""]
         columns_dict[column]=dict(zip(unique,range(len(unique))))
         columns_dict_len[column]=len(unique)
         new_columns+=[column+"&&"+str(c) for c in unique]
 
 
+
+
     # Logistic Regression
     print("Logistic Regression")
+    df_feature_train.unpersist()
+    df_feature_test.unpersist()
+
+    _time = time.time()
     parsedData = df_train.rdd.map(rowTransform).toDF()
+    print("rowTransform: ", str(time.time() - _time) + "s")
     lr = LogisticRegression(maxIter=10, regParam=0.0, elasticNetParam=0.0)
+    _time = time.time()
     lrModel = lr.fit(parsedData)
+    print("LR: ", str(time.time() - _time) + "s")
     if mode != "train_test":
         trainingSummary = lrModel.summary
         print("areaUnderROC: " + str(trainingSummary.areaUnderROC))
